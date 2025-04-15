f58f3c06acae7157c1e5656cf83d18740805dd69
Simon <63975668+Simyon264@users.noreply.github.com>
2025-04-15 20:38:53 +0200

    Implement admin claim authentication

--- SS14.Jetfish/Security/AccessAreaAuthorizationHandler.cs
+++ SS14.Jetfish/Security/AccessAreaAuthorizationHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using SS14.Jetfish.Configuration;
 using SS14.Jetfish.Database;
 using SS14.Jetfish.Helpers;
 
@@ -7,16 +8,26 @@ namespace SS14.Jetfish.Security;
 public sealed class AccessAreaAuthorizationHandler : AuthorizationHandler<AccessAreaAuthorizationRequirement>
 {
     private readonly IServiceScopeFactory _serviceScopeFactory; // Required as this is a singleton
+    private readonly ServerConfiguration _serverConfiguration = new();
 
-    public AccessAreaAuthorizationHandler(IServiceScopeFactory serviceScopeFactory)
+    public AccessAreaAuthorizationHandler(IServiceScopeFactory serviceScopeFactory, IConfiguration configuration)
     {
         _serviceScopeFactory = serviceScopeFactory;
+        configuration.Bind(ServerConfiguration.Name, _serverConfiguration);
     }
 
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AccessAreaAuthorizationRequirement requirement)
     {
         using var scope     = _serviceScopeFactory.CreateScope();
         using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        // Admin claim is for first setup, it has access to everything
+        if (!string.IsNullOrEmpty(_serverConfiguration.AdminClaim)
+            && context.User.HasClaim(c => c.Type == _serverConfiguration.AdminClaim))
+        {
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
 
         // TODO: FINISH
 
