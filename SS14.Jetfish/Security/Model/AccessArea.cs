283369cfa49eb735bc50a315c2fa64f562b6466a
Simon <63975668+Simyon264@users.noreply.github.com>
2025-04-16 16:10:35 +0200

    Add admin global policy areas

--- SS14.Jetfish/Security/Model/AccessArea.cs
+++ SS14.Jetfish/Security/Model/AccessArea.cs
@@ -1,9 +1,16 @@
 ﻿namespace SS14.Jetfish.Security.Model;
 
-public enum AccessArea
+// When adding new values, ensure that none of the assigned values are changed, DB stores only the numerical value.
+public enum AccessArea : short
 {
-    TeamCreate,
-    TeamEdit,
-    TeamDelete,
-    ProjectRead
+    // Assign every "area" with its own 10th digit.
+
+    AdminReadGlobalPolicies = 1,
+    AdminWriteGlobalPolicies = 2,
+
+    TeamCreate = 10,
+    TeamEdit = 11,
+    TeamDelete = 12,
+
+    ProjectRead = 20,
 }
