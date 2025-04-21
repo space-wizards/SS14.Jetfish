6749456035f190d3f3604a667037d3802ad8c0b7
Simon <63975668+Simyon264@users.noreply.github.com>
2025-04-21 13:30:17 +0200

    Add skeleton image loading

--- SS14.Jetfish/Components/Shared/ProjectCards/ProjectCard.razor.cs
+++ SS14.Jetfish/Components/Shared/ProjectCards/ProjectCard.razor.cs
@@ -9,17 +9,28 @@ public partial class ProjectCard : ComponentBase
     [Parameter]
     public required Project Project { get; set; }
 
-    private string GetBackground()
+    private bool _isImageLoaded = false;
+    protected override void OnParametersSet()
     {
         Validator.ValidateObject(Project, new ValidationContext(Project), true);
-        
-        if (Project.BackgroundSpecifier == ProjectBackgroundSpecifier.Color)
+        base.OnParametersSet();
+    }
+
+    private string GetBackground()
+    {
+        if (!_isImageLoaded && Project.BackgroundSpecifier == ProjectBackgroundSpecifier.Image)
         {
-            return $"background: {Project.Background};";
+            return "background-color: #000;";
         }
-        else
-        {
-            return $"background: center / cover url(\"project-file/{Project.Id}/file/{Project.Background}\")";
-        }
+
+        return Project.BackgroundSpecifier == ProjectBackgroundSpecifier.Color
+            ? $"background: {Project.Background};"
+            : $"background: center / cover url(\"project-file/{Project.Id}/file/{Project.Background}\")";
+    }
+
+    private void OnImageLoaded()
+    {
+        _isImageLoaded = true;
+        StateHasChanged(); // trigger re-render
     }
 }
