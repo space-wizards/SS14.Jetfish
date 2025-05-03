b899ad40371fa38229193201355c8346ffb8d59f
Simon <63975668+Simyon264@users.noreply.github.com>
2025-05-03 22:00:57 +0200

    Move UserAvatar into its own component

---
+++ SS14.Jetfish/Components/Pages/User/UserAvatar.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace SS14.Jetfish.Components.Pages.User;
+
+public partial class UserAvatar : ComponentBase
+{
+    [CascadingParameter]
+    public Security.Model.User? User { get; set; }
+    private string UserProfilePictureLink => $"global-file/{User!.ProfilePicture}";
+
+    [Parameter]
+    public Size Size { get; set; } = Size.Medium;
+}
