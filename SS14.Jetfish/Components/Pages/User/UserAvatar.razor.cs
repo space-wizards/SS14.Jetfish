70f5ffc51c70ff6e752caa004b66794877d55fc1
Simon <63975668+Simyon264@users.noreply.github.com>
2025-05-04 08:52:44 +0200

    Use UserAvatar everywhere

--- SS14.Jetfish/Components/Pages/User/UserAvatar.razor.cs
+++ SS14.Jetfish/Components/Pages/User/UserAvatar.razor.cs
@@ -7,8 +7,26 @@ public partial class UserAvatar : ComponentBase
 {
     [CascadingParameter]
     public Security.Model.User? User { get; set; }
-    private string UserProfilePictureLink => $"global-file/{User!.ProfilePicture}";
+
+    private string UserProfilePictureLink()
+    {
+        return UserOverride != null ? $"global-file/{UserOverride.ProfilePicture}" : $"global-file/{User!.ProfilePicture}";
+    }
+
+    private string GetTooltip()
+    {
+        return UserOverride != null ? UserOverride.DisplayName : User!.DisplayName;
+    }
 
     [Parameter]
     public Size Size { get; set; } = Size.Medium;
+
+    /// <summary>
+    /// If set, this user is used instead of <see cref="User"/>
+    /// </summary>
+    [Parameter]
+    public Security.Model.User? UserOverride { get; set; }
+
+    [Parameter]
+    public bool Tooltip { get; set; } = false;
 }
