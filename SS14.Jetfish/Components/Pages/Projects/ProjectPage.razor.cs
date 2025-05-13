e920757a4760650a15f89b9b38fadc1a660c5009
Simon <63975668+Simyon264@users.noreply.github.com>
2025-05-13 18:46:04 +0200

    WIP: Project Page

--- SS14.Jetfish/Components/Pages/Projects/ProjectPage.razor.cs
+++ SS14.Jetfish/Components/Pages/Projects/ProjectPage.razor.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 using SS14.Jetfish.Components.Layout;
@@ -26,6 +27,35 @@ public partial class ProjectPage : ComponentBase
             return;
 
         Project = await Context.Project.SingleOrDefaultAsync(p => p.Id == ProjectId);
+        if (Project != null)
+        {
+            Validator.ValidateObject(Project, new ValidationContext(Project), true);
+            if (Project.BackgroundSpecifier == ProjectBackgroundSpecifier.Color)
+                _displaySkeleton = false;
+        }
+
         StateHasChanged();
+    }
+
+    private bool _displaySkeleton = true;
+    private bool _isImageLoaded = false;
+
+    private void OnImageLoaded()
+    {
+        _isImageLoaded = true;
+        _displaySkeleton = false;
+        StateHasChanged(); // trigger re-render
+    }
+
+    private string GetBackground()
+    {
+        if (!_isImageLoaded && Project!.BackgroundSpecifier == ProjectBackgroundSpecifier.Image)
+        {
+            return "background-color: #000;";
+        }
+
+        return Project!.BackgroundSpecifier == ProjectBackgroundSpecifier.Color
+            ? $"background: {Project.Background};"
+            : $"background: center / cover url(\"project-file/{Project.Id}/file/{Project.Background}\")";
     }
 }
