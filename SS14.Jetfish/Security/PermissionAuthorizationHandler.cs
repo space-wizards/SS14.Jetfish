e4381fd6827205320fdfdcffa66ebf53a8d9a393
juliangiebel <juliangiebel@live.de>
2025-06-20 21:05:20 +0200

    Change auth handler to use permission claims

--- SS14.Jetfish/Security/PermissionAuthorizationHandler.cs
+++ SS14.Jetfish/Security/PermissionAuthorizationHandler.cs
@@ -45,73 +45,13 @@ public sealed class PermissionAuthorizationHandler : AuthorizationHandler<Permis
             _ => null
         };
 
-        #region Idp Roles
-
-        var hasIdpAccess = await dbContext.HasIdpAccess(_serverConfiguration, context.User, resourceId, requirement.Permissions.ToArray());
-
-        if (hasIdpAccess)
-        {
-            context.Succeed(requirement);
-            return;
-        }
-
-        #endregion
-
-        #region User
-        var user = await dbContext.User
-            .Include(user => user.ResourcePolicies)
-            .ThenInclude(resourcePolicy => resourcePolicy.AccessPolicy)
-            .SingleOrDefaultAsync(user => user.Id == context.User.Claims.GetUserId());
-
-        if (user == null)
-        {
-            context.Fail(new AuthorizationFailureReason(this, "User not found in database (Not logged in?)"));
-            return;
-        }
-
-
-        if (user.ResourcePolicies
-            .Any(policy => (policy.ResourceId == resourceId || policy.Global)
-                    && requirement.Permissions.Intersect(policy.AccessPolicy.Permissions).Any()))
-        {
-            context.Succeed(requirement);
-            return;
-        }
-        #endregion
-
-        var hasTeamAccess = dbContext.TeamMember
-            .Include(teamMember => teamMember.Role)
-            .ThenInclude(role => role.Policies)
-            .ThenInclude(policy => policy.AccessPolicy)
-            .Where(teamMember => teamMember.UserId == user.Id)
-            .Any(teamMember => teamMember.Role.Policies
-                .Where(policy => policy.ResourceId == resourceId || policy.Global)
-                .Any(policy => requirement.Permissions.Intersect(policy.AccessPolicy.Permissions).Any()));
-
-        if (hasTeamAccess)
+        if (context.User.HasPermissions(requirement.Permissions, global: true)
+            || context.User.HasPermissions(requirement.Permissions, resourceId))
         {
             context.Succeed(requirement);
             return;
         }
 
         context.Fail(new AuthorizationFailureReason(this, "User doesn't have access"));
-        /*
-         * 1. Check if user is superadmin (DONE)
-           2. Check if the user has been granted access directly
-           3. Check one of the users team memberships contains the required access policy
-           4. Cache the result
-
-            Proposal to make querying for permissions less complicated:
-            Create a table that contains an array of allowed actions (Permissions) per user and resource (resource = null for resourceless actions)
-            Said table needs to be kept up to date through triggers on authorization related tables.
-            Changes need to be normalized to added and removed actions per resource.
-            This is better than the above because changes to permissions occur much less frequent than querying if a user has permissions for something
-            Triggers would need to be implemented for the following tables:
-            - User_ResourcePolicies
-            - TeamMember
-            - Role_Policies
-            - AccessPolicy
-            An alternative to triggers would be a materialized view but refreshing that view would mean it gets rebuild completely every time some permission changes
-         */
     }
 }
